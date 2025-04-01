c04ebbd1fdd06005432323aec1d6df275cc87443
MANISHA CHAUHAN <88399187+chauhanmanisha24@users.noreply.github.com>
2025-04-01 19:25:30 +0530

    Add files via upload

---
+++ NEWWIRINGSYSTEM-main/form-handler.php
@@ -0,0 +1,29 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+// $email_form = 'yourwebsite@yahoo.com';
+
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name:$name.\n".
+                "User Email:$visitor_email.\n".
+                "Subject:$subject.\n".
+                "User Message:$message.\n";
+
+$to =  'newwiringsystem@yahoo.com';
+
+$headers = "From:yourwebsite@.com\r\n";
+
+$headers .= "Reply-To:$visitor_email \r\n";
+              
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location:contact.html");
+
+
+?>
